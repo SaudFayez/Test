c357f40c2762b866ae212833d7d0de35d68274fb
saudfayez <saud@batchroastery.com>
2026-04-29 01:11:56 +0300

    Delete delivery_smsa_egs directory

--- delivery_smsa_egs/models/stock_picking.py
+++
@@ -1,8 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import fields, models
-
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    cancel_reason = fields.Char()
